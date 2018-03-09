47f529b00f0dafccaf8acfe6febbc9483e8e1f86
KarneyenkaDzmitry <33957610+KarneyenkaDzmitry@users.noreply.github.com>
2018-03-10 00:09:54 +0300

    Unit tests of Triangle class

--- src/test/java/triangle/TriangleTestJUnit.java
+++ src/test/java/triangle/TriangleTestJUnit.java
@@ -1,6 +1,10 @@
 package triangle;
 
-import org.junit.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
@@ -12,14 +16,35 @@ import static org.junit.Assert.*;
 
 @RunWith(value = Parameterized.class)
 public class TriangleTestJUnit {
-    private String marker;
-    private Triangle triangle;
+    private String markerOfTest;//PositiveTest or NegativeTest
+    private String message;//Message for failed tests
+    private Triangle triangle;//Object of the testing class
     private double a;//The first side of triangle
     private double b;//The second side of triangle
     private double c;//The third side of triangle
     private double expectedOfDetection;//The expected result of the method detectTriangle()
     private double expectedSquare;//The expected result of getSquare() of the triangle. If triangle doesn't exist
     //the parameter equals zero
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    private String setErrorMessage() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("\"This is ").append(markerOfTest).append(" with parameters: a=[").append(a).append("], b=[").append(b).append("], c=[");
+        builder.append(c).append("], \n");
+        if (expectedOfDetection == 0.0) {
+            builder.append("expected of detectionTriangle()=[Exception], ");
+        } else {
+            int exp = (int) expectedOfDetection;
+            builder.append("expected of detectionTriangle()=[").append(exp).append("], ");
+        }
+        if (expectedSquare == 0.0) {
+            builder.append("expected of getSquare()=[Exception], ");
+        } else {
+            builder.append("expected of detectionTriangle()=[").append(expectedSquare).append("].\"");
+        }
+        return builder.toString();
+    }
 
     @Parameterized.Parameters
     public static Collection<Object[]> set_of_parameters() {
@@ -30,117 +55,131 @@ public class TriangleTestJUnit {
         //expectedOfDetection - expected result working of detectRectangle();
         //expectedSquare - expected resul of calling getSquare() method.
         return Arrays.asList(new Object[][]{
-                {"Прямоугольный", new ArrayList<Double>(Arrays.asList(3.0, 4.0, 5.0, 8.0, 6.0))},
-                {"Прямоугольный", new ArrayList<Double>(Arrays.asList(65.00, 72.00, 97.00, 8.0, 2340.00))},
-                {"Прямоугольный", new ArrayList<Double>(Arrays.asList(68.00, 285.00, 293.00, 8.0, 9690.00))},
-                {"Равнобедренный", new ArrayList<Double>(Arrays.asList(3.00, 3.00, 3*1.4142135623730950488016887242097, 10.0, 4.50))},
-                {"Равнобедренный", new ArrayList<Double>(Arrays.asList(3.00, 3.00, 5.00, 2.0, 4.14578098794425))},
-                {"Равнобедренный", new ArrayList<Double>(Arrays.asList(10.00, 10.00, 15.00, 2.0, 49.607837082461074))},
-                {"Равнобедренный", new ArrayList<Double>(Arrays.asList(97.00, 97.00, 123.00, 2.0, 4613.217444203124))},
-                {"Равнобедренный", new ArrayList<Double>(Arrays.asList(3.00, 3.00, 2.00, 2.0, 2.8284271247461903))},
-                {"Равнобедренный", new ArrayList<Double>(Arrays.asList(10.00, 10.00, 9.00, 2.0, 40.18628497385644))},
-                {"Равнобедренный", new ArrayList<Double>(Arrays.asList(99.00, 99.00, 94.00, 2.0, 4095.2079312288893))},
-                {"Равносторонний", new ArrayList<Double>(Arrays.asList(5.00, 5.00, 5.00, 3.0, 10.825317547305483))},
-                {"Равносторонний", new ArrayList<Double>(Arrays.asList(17.00, 17.00, 17.00, 3.0, 125.14067084685138))},
-                {"Равносторонний", new ArrayList<Double>(Arrays.asList(111.00, 111.00, 111.00, 3.0, 5335.149500014034))},
-                {"Равносторонний", new ArrayList<Double>(Arrays.asList(1.00, 1.00, 1.00, 3.0, 0.4330127018922193))},
-                {"Разносторонний", new ArrayList<Double>(Arrays.asList(10.00,	6.00,	5.00, 4.00, 11.399013115177997))},
-                {"Разносторонний", new ArrayList<Double>(Arrays.asList(26.00, 31.00, 12.00, 4.00, 151.96525096218542))},
-                {"Разносторонний", new ArrayList<Double>(Arrays.asList(113.00, 158.00, 256.00, 4.00, 5601.6429230628))},
-                {"Несуществующий", new ArrayList<Double>(Arrays.asList(10.00, 3.00, 2.00, 0.00, Double.NaN))},
-                {"Несуществующий", new ArrayList<Double>(Arrays.asList(5.00, 12.00, 4.00, 0.00, Double.NaN))},
-                {"Несуществующий", new ArrayList<Double>(Arrays.asList(20.00, 20.00, 51.00, 0.00, Double.NaN))},
-                {"Несуществующий", new ArrayList<Double>(Arrays.asList(1.00, 1.00, 0.00, 0.00, 0.0))},
-                {"Несуществующий", new ArrayList<Double>(Arrays.asList(1.00, 0.00, 1.00, 0.00, 0.0))},
-                {"Несуществующий", new ArrayList<Double>(Arrays.asList(0.00, 1.00, 1.00, 0.00, 0.0))},
-                {"Несуществующий", new ArrayList<Double>(Arrays.asList(-5.00, 4.00, 3.00, 0.00, 6.0))},
-                {"Несуществующий", new ArrayList<Double>(Arrays.asList(4.00, 3.00, -5.00, 0.00, 6.0))},
-                {"Несуществующий", new ArrayList<Double>(Arrays.asList(3.00, -5.00, 4.00, 0.00, 6.0))},
-                {"Несуществующий", new ArrayList<Double>(Arrays.asList(10.00, 3.00, 2.00, 0.00, Double.NaN))},
-                {"Несуществующий", new ArrayList<Double>(Arrays.asList(5.00, 5.00, 12.00, 0.00, Double.NaN))},
-                {"Равнобедренный", new ArrayList<Double>(Arrays.asList(Double.MAX_VALUE, Double.MAX_VALUE, 1.00, 2.00, Double.POSITIVE_INFINITY))},
-                {"Несуществующий", new ArrayList<Double>(Arrays.asList(Double.MAX_VALUE, 500.00, 400.00, 0.00, Double.NaN))},
-                {"Разносторонний", new ArrayList<Double>(Arrays.asList(Double.MAX_VALUE, Double.MAX_VALUE - 1.00,Double.MAX_VALUE - 2.00, 4.00, Double.POSITIVE_INFINITY))},
-                {"Равносторонний", new ArrayList<Double>(Arrays.asList(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE, 3.00, Double.POSITIVE_INFINITY))},
-                {"Равнобедренный", new ArrayList<Double>(Arrays.asList(Double.NaN, 5.00, 5.00, 2.00, Double.NaN))},
-                {"Равнобедренный", new ArrayList<Double>(Arrays.asList(Double.POSITIVE_INFINITY, 5.00, 5.00, 2.00, Double.NaN))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(3.0, 4.0, 5.0, 8.0, 6.0))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(5.0, 3.0, 4.0, 8.0, 6.0))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(4.0, 5.0, 3.0, 8.0, 6.0))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(65.00, 72.00, 97.00, 8.0, 2340.00))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(68.00, 285.00, 293.00, 8.0, 9690.00))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(3.00, 3.00, 3 * 1.4142135623730950488016887242097, 10.0, 4.50))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(3.00, 3.00, 5.00, 2.0, 4.14578098794425))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(10.00, 10.00, 15.00, 2.0, 49.607837082461074))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(97.00, 97.00, 123.00, 2.0, 4613.217444203124))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(3.00, 3.00, 2.00, 2.0, 2.8284271247461903))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(10.00, 10.00, 9.00, 2.0, 40.18628497385644))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(99.00, 99.00, 94.00, 2.0, 4095.2079312288893))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(5.00, 5.00, 5.00, 3.0, 10.825317547305483))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(17.00, 17.00, 17.00, 3.0, 125.14067084685138))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(111.00, 111.00, 111.00, 3.0, 5335.149500014034))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(1.00, 1.00, 1.00, 3.0, 0.4330127018922193))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(10.00, 6.00, 5.00, 4.00, 11.399013115177997))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(26.00, 31.00, 12.00, 4.00, 151.96525096218542))},
+                {"Positive Test", new ArrayList<>(Arrays.asList(113.00, 158.00, 256.00, 4.00, 5601.6429230628))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(10.00, 3.00, 2.00, 0.00, 0.00))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(5.00, 12.00, 4.00, 0.00, 0.00))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(20.00, 20.00, 51.00, 0.00, 0.00))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(1.00, 1.00, 0.00, 0.00, 0.0))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(1.00, 0.00, 1.00, 0.00, 0.0))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(0.00, 1.00, 1.00, 0.00, 0.0))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(0.00, 0.00, 0.00, 0.00, 0.0))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(-5.00, 4.00, 3.00, 0.00, 0.00))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(4.00, 3.00, -5.00, 0.00, 0.00))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(3.00, -5.00, 4.00, 0.00, 0.00))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(10.00, 3.00, 2.00, 0.00, 0.00))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(5.00, 5.00, 12.00, 0.00, 0.00))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(Double.MAX_VALUE, Double.MAX_VALUE, 1.00, 0.00, 0.00))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(Double.MAX_VALUE, 500.00, 400.00, 0.00, 0.00))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(Double.MAX_VALUE, Double.MAX_VALUE - 1.00, Double.MAX_VALUE - 2.00, 0.00, 0.00))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE, 0.00, 0.00))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(Double.NaN, 5.00, 5.00, 0.00, 0.00))},
+                {"NegativeTest", new ArrayList<>(Arrays.asList(Double.POSITIVE_INFINITY, 5.00, 5.00, 0.00, 0.0))},
         });
     }
 
-    public TriangleTestJUnit(String marker, ArrayList<Double> list) {
-        this.marker = marker;
+    public TriangleTestJUnit(String markerOfTest, ArrayList<Double> list) {
+        this.markerOfTest = markerOfTest;
         this.a = list.get(0);
         this.b = list.get(1);
         this.c = list.get(2);
         this.expectedOfDetection = list.get(3);
         this.expectedSquare = list.get(4);
+        this.message = setErrorMessage();
     }
-    /*@BeforeClass
-    public static void tst_BeforeClassAnnotation() {
-        System.out.println("Test of @BeforeClass annotation");
-    }
-    @AfterClass
-    public static void tst_AfterClassAnnotation() {
-        System.out.println("Test of @AfterClass annotation");
-    }*/
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         triangle = new Triangle(a, b, c);
-        //System.out.println("Test of @Before annotation");
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
         triangle = null;
-        //System.out.println("Test of @After annotation");
-
     }
-
 
     @Test
     public void tst_getMessage() {
-        if (triangle.checkTriangle()) {
+        triangle.checkTriangle();
+        if (markerOfTest.equals("PositiveTest")) {
             assertNotNull(triangle.getMessage());
-            assertEquals("", triangle.getMessage());
+            assertEquals(message, "", triangle.getMessage());
         } else {
+            boolean fail = false;
             if (a <= 0) {
-                assertEquals("a<=0", triangle.getMessage());
+                assertEquals(message, "a<=0", triangle.getMessage());
+                fail = true;
             }
-            if (b <= 0) {
-                assertEquals("b<=0", triangle.getMessage());
+            if ((b <= 0) && (!fail)) {
+                assertEquals(message, "b<=0", triangle.getMessage());
+                fail = true;
             }
-            if (c <= 0) {
-                assertEquals("c<=0", triangle.getMessage());
+            if ((c <= 0) && (!fail)) {
+                assertEquals(message, "c<=0", triangle.getMessage());
+                fail = true;
             }
-            if (a + b <= c) {
-                assertEquals("a+b<=c", triangle.getMessage());
+            if ((a + b <= c) && (!fail)) {
+                assertEquals(message, "a+b<=c", triangle.getMessage());
+                fail = true;
             }
-            if (a + c <= b) {
-                assertEquals("a+c<=b", triangle.getMessage());
+            if ((a + c <= b) && (!fail)) {
+                assertEquals(message, "a+c<=b", triangle.getMessage());
+                fail = true;
             }
-            if (b + c <= a) {
-                assertEquals("b+c<=a", triangle.getMessage());
+            if ((b + c <= a) && (!fail)) {
+                assertEquals(message, "b+c<=a", triangle.getMessage());
+                fail = true;
+            }
+            if (!fail) {
+                assertNotNull(triangle.getMessage());
+                assertEquals(message, "", triangle.getMessage());
             }
         }
     }
 
     @Test
     public void tst_checkTriangle() {
-        if (marker.equals("Несуществующий")) {
-            assertFalse(triangle.checkTriangle());
+        if (markerOfTest.equals("NegativeTest")) {
+            assertFalse(message, triangle.checkTriangle());
         } else {
-            assertTrue(triangle.checkTriangle());
+            assertTrue(message, triangle.checkTriangle());
         }
     }
 
     @Test
     public void tst_detectTriangle() {
-        assertEquals(expectedOfDetection, (double) triangle.detectTriangle(), 0.1);
+        if (markerOfTest.equals("NegativeTest")) {
+            exception.expect(Exception.class);
+            triangle.detectTriangle();
+        } else {
+            assertEquals(message, expectedOfDetection, (double) triangle.detectTriangle(), 0.1);
+        }
     }
 
     @Test
     public void tst_getSquare() {
-        assertEquals(expectedSquare, triangle.getSquare(), 0.000001);
+        if (markerOfTest.equals("NegativeTest")) {
+            exception.expect(Exception.class);
+            triangle.getSquare();
+        } else {
+            assertEquals(message, expectedSquare, triangle.getSquare(), 0.000000001);
+        }
     }
 }
--- src/test/java/triangle/TriangleTestNG.java
+++ src/test/java/triangle/TriangleTestNG.java
@@ -1,34 +1,139 @@
 package triangle;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class TriangleTestNG {
 
-    @BeforeMethod
-    public void setUp() throws Exception {
+    @DataProvider(name = "negativeTests")
+    public Object[][] createNegativeData() {
+        return new Object[][]{
+                {0.0, 0.0, 0.0},
+                {10.0, 3.0, 2.0},
+                {5.0, 12.0, 4.0},
+                {20.0, 20.0, 51.0},
+                {1.0, 1.0, 0.0},
+                {1.0, 0.0, 1.0},
+                {0.0, 1.0, 1.0},
+                {-5.0, 4.0, 3.0},
+                {4.0, 3.0, -5.0},
+                {3.0, -5.0, 4.0},
+                {5.0, 5.0, 12.0},
+                {Double.MAX_VALUE, Double.MAX_VALUE, 1.0},
+                {Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE},
+                //{Double.MAX_VALUE, 500, 400},
+                //{400.0, Double.MAX_VALUE, 500},
+                //{Double.NaN, 500, 500},
+                //{Double.POSITIVE_INFINITY, 500, 500},
+        };
     }
 
-    @AfterMethod
-    public void tearDown() throws Exception {
+    @Test(dataProvider = "negativeTests")
+    public void testGetMessageNegative(Double a, Double b, Double c) {
+        Triangle triangle = new Triangle(a, b, c);
+        triangle.checkTriangle();
+        boolean fail = false;
+        if (a <= 0) {
+            assertEquals(triangle.getMessage(), "a<=0");
+            fail = true;
+        }
+        if ((b <= 0) && (!fail)) {
+            assertEquals(triangle.getMessage(), "b<=0");
+            fail = true;
+        }
+        if ((c <= 0) && (!fail)) {
+            assertEquals(triangle.getMessage(), "c<=0");
+            fail = true;
+        }
+        if ((a + b <= c) && (!fail)) {
+            assertEquals(triangle.getMessage(), "a+b<=c");
+            fail = true;
+        }
+        if ((a + c <= b) && (!fail)) {
+            assertEquals(triangle.getMessage(), "a+c<=b");
+            fail = true;
+        }
+        if ((b + c <= a) && (!fail)) {
+            assertEquals(triangle.getMessage(), "b+c<=a");
+            fail = true;
+        }
+        if (!fail) {
+            assertNotNull(triangle.getMessage());
+            assertEquals(triangle.getMessage(), "");
+        }
     }
 
-    @Test
-    public void testGetMessage() throws Exception {
+    @Test(dataProvider = "negativeTests")
+    public void testCheckTriangleNegative(Double a, Double b, Double c) {
+        Triangle triangle = new Triangle(a, b, c);
+        assertFalse(triangle.checkTriangle());
     }
 
-    @Test
-    public void testCheckTriangle() throws Exception {
+    @Test(dataProvider = "negativeTests", expectedExceptions = Exception.class)
+    public void testDetectTriangleNegative(Double a, Double b, Double c) {
+        Triangle triangle = new Triangle(a, b, c);
+        triangle.detectTriangle();
     }
 
-    @Test
-    public void testDetectTriangle() throws Exception {
+    @Test(dataProvider = "negativeTests", expectedExceptions = Exception.class)
+    public void testGetSquareNegative(Double a, Double b, Double c) {
+        Triangle triangle = new Triangle(a, b, c);
+        triangle.getSquare();
     }
 
-    @Test
-    public void testGetSquare() throws Exception {
+    @DataProvider(name = "positiveTests")
+    public Object[][] createPositiveData() {
+        return new Object[][]{
+                {3.0, 4.0, 5.0, 8.0, 6.0},
+                {5.0, 3.0, 4.0, 8.0, 6.0},
+                {4.0, 5.0, 3.0, 8.0, 6.0},
+                {65.0, 72.0, 97.0, 8.0, 2340.00},
+                {68.0, 285.0, 293.0, 8.0, 9690.00},
+                {293.0, 68.0, 285.0, 8.0, 9690.00},
+                {285.0, 293.0, 68.0, 8.0, 9690.00},
+                {3.0, 3.0, 4.242640687, 10.0, 4.5},
+                {3.0, 3.0, 5.0, 2.0, 4.14578098794425},
+                {10.0, 10.0, 15.00, 2.0, 49.607837082461074},
+                {97.0, 97.0, 123.00, 2.0, 4613.217444203124},
+                {3.0, 3.0, 2.00, 2.0, 2.8284271247461903},
+                {10.0, 10.0, 9.00, 2.0, 40.18628497385644},
+                {99.0, 99.0, 94.00, 2.0, 4095.2079312288893},
+                {5.0, 5.0, 5.00, 3.0, 10.825317547305483},
+                {17.0, 17.0, 17.00, 3.0, 125.14067084685138},
+                {111.0, 111.0, 111.00, 3.0, 5335.149500014034},
+                {1.0, 1.0, 1.00, 3.0, 0.4330127018922193},
+                {10.0, 6.0, 5.00, 4.0, 11.399013115177997},
+                {26.0, 31.0, 12.00, 4.0, 151.96525096218542},
+                {113.0, 158.0, 256.00, 4.0, 5601.64292306284},
+                {753.12345678, 987.65432198, 456.78945612, 4.0, 164611.65116652913},
+        };
+    }
+
+    @Test(dataProvider = "positiveTests")
+    public void testGetMessagePositive(Double a, Double b, Double c, Double expectedDetection, Double ExpectedSquare) {
+        Triangle triangle = new Triangle(a, b, c);
+        triangle.checkTriangle();
+        assertNotNull(triangle.getMessage());
+        assertEquals(triangle.getMessage(), "");
+    }
+
+    @Test(dataProvider = "positiveTests")
+    public void testCheckTrianglePositive(Double a, Double b, Double c, Double expectedDetection, Double ExpectedSquare) {
+        Triangle triangle = new Triangle(a, b, c);
+        assertTrue(triangle.checkTriangle());
+    }
+
+    @Test(dataProvider = "positiveTests")
+    public void testDetectTrianglePositive(Double a, Double b, Double c, Double expectedDetection, Double ExpectedSquare) {
+        Triangle triangle = new Triangle(a, b, c);
+        assertEquals((double) triangle.detectTriangle(), expectedDetection);
+    }
+
+    @Test(dataProvider = "positiveTests")
+    public void testGetSquarePositive(Double a, Double b, Double c, Double expectedDetection, Double ExpectedSquare) {
+        Triangle triangle = new Triangle(a, b, c);
+        assertEquals(triangle.getSquare(), ExpectedSquare);
     }
 }
